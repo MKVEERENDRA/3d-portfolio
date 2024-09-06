c1c6904c64ecb14ade514f0c95d67439a17146d5
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2024-09-06 20:29:08 +0530

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -24,9 +24,9 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar hide={hide} />
-          <Hero />
 
         </div>
+        <Hero/>
         <About />
         <Experience />
         <Tech />
