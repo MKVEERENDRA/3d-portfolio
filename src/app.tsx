29a237b8134a60be0870b729c87a0c6954325ce0
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2024-09-06 20:32:04 +0530

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,47 +1,27 @@
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
-import Banner from "./components/banner";
-import Footer from "./components/footer";
 
-// App
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
 const App = () => {
-  const [hide, setHide] = useState(true);
-
   return (
     <BrowserRouter>
-      <Banner hide={hide} setHide={setHide} />
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar hide={hide} />
-
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
         </div>
-        <Hero/>
         <About />
         <Experience />
         <Tech />
         <Works />
         <Feedbacks />
-
-        {/* Contact */}
-        <div className="relative z-0">
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
-        <Footer />
       </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
