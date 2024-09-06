5173cfd7882932e22786e57a3b907e291f778234
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2024-09-06 20:27:10 +0530

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,39 +1,48 @@
-import React from "react";
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
 import { motion } from "framer-motion";
-import { personalInfo } from "../constants";
 
-const Hero = () => {
+import { ComputersCanvas } from "./canvas";
+import { styles } from "../styles";
+import { cn } from "../utils/lib";
+
+// Hero
+export const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 inset-0`}
+        className={cn(
+          styles.paddingX,
+          "absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5",
+        )}
       >
+        {/* Title */}
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-electric-purple" />
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
+        {/* About Me */}
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm{" "}
-            <span className="text-electric-purple">{personalInfo.name}</span>
+          <h1 className={cn(styles.heroHeadText, "text-white")}>
+            Hi, I'm <span className="text-[#915eff]">Veerendra </span>
           </h1>
-          <p className={`${styles.heroSubText} text-white-100 mt-2`}>
-            Innovative {personalInfo.role}, <br className="sm:block hidden" />
-            building web and mobile applications
+          <p className={cn(styles.heroSubText, "mt-2 text-white-100")}>
+            I develop 3D visuals, user <br className="sm:block hidden" />
+            interfaces and Web3 developer
           </p>
         </div>
       </div>
 
+      {/* Computer Model */}
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-2 bottom-12 w-full flex justify-center items-center">
+      {/* Scroll to about section */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
-              animate={{ y: [0, 24, 0] }}
+              animate={{
+                y: [0, 24, 0],
+              }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
@@ -47,5 +56,3 @@ const Hero = () => {
     </section>
   );
 };
-
-export default Hero;
