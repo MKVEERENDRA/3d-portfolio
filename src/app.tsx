c2f4ad09c6c2e30142b87c35724d067fd93457b8
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2024-09-06 20:15:05 +0530

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,31 @@
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
-import Banner from "./components/banner";
-import Footer from "./components/footer";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Tech from "./components/Tech";
+import Works from "./components/Works";
+import Contact from "./components/Contact";
+import { StarsCanvas } from "./components/canvas";
+import HireMe from "./components/HireMe";
 
-// App
 const App = () => {
-  const [hide, setHide] = useState(true);
-
   return (
     <BrowserRouter>
-      <Banner hide={hide} setHide={setHide} />
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar hide={hide} />
+          <Navbar />
           <Hero />
-
         </div>
         <About />
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
-
-        {/* Contact */}
         <div className="relative z-0">
           <Contact />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
-        <Footer />
+        <HireMe />
       </div>
     </BrowserRouter>
   );
